34cb12c82d781edf4aeeab418e9f9537696d6402
mrzamay <timofeev.ivaan@gmail.com>
2025-07-17 17:41:09 +0300

    Поправлено форматирование кода

--- www/public/php-basics/2.php
+++ www/public/php-basics/2.php
@@ -1,22 +1,22 @@
 <?php
-    class Employee {
-        public $name;
-        public $age;
-        public $salary;
+class Employee {
+    public $name;
+    public $age;
+    public $salary;
 
-    }
+}
 
-    $user1 = new Employee();
-    $user1->name = 'John';
-    $user1->age = 25;
-    $user1->salary = 1000;
+$user1 = new Employee();
+$user1->name = 'John';
+$user1->age = 25;
+$user1->salary = 1000;
 
-    $user2 = new Employee();
-    $user2->name = 'Eric';
-    $user2->age = 26;
-    $user2->salary = 2000;
+$user2 = new Employee();
+$user2->name = 'Eric';
+$user2->age = 26;
+$user2->salary = 2000;
 
-    $salarySum = $user1->salary + $user2->salary;
-    $ageSum = $user1->age + $user2->age;
+$salarySum = $user1->salary + $user2->salary;
+$ageSum = $user1->age + $user2->age;
 
-    print_r("Salary sum: {$salarySum}\nAge sum: {$ageSum}");
+print_r("Salary sum: {$salarySum}\nAge sum: {$ageSum}");
